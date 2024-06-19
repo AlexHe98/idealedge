bd35f753e7dac7f258d819e64c066b43ecf39373
alex-he <mr.a.he98@gmail.com>
2024-06-19 14:42:22 +1000

    Use new tracker functionality to make experiment output more useful.

--- experiment.py
+++ experiment.py
@@ -4,7 +4,7 @@ Perform knot decomposition experiments in bulk.
 from sys import argv
 from timeit import default_timer
 from regina import *
-from decomposeknot import decompose
+from decomposeknot import decompose, DecompositionTracker
 
 
 def clearExperiments(packet):
@@ -42,25 +42,26 @@ def decomposeAll( packet, verbose=True, insertChildren=False ):
     return
 
 
-def decomposeAllInTable( filename, verbose=True ):
+def decomposeAllInTable(filename):
     """
-    Decomposes all knots listed in the table in the given file.
+    Decomposes all knots listed in the table in the given file, and prints
+    the results to standard output.
 
     The given file should be a CSV file that includes (at least) the
     following data:
     --> a column of knot names under the heading "name"; and
     --> a column of knot signatures under the heading "knot_sig".
-
-    With the verbose option (which is switched on by default), this routine
-    prints regular reports on the progress of the experiments.
     """
-    if verbose:
-        name = filename.split( "/" )[-1].split( "." )[0]
-        print(name)
-        print( "="*len(name) )
-        print()
-        knotCount = 0
-        start = default_timer()
+    title = filename.split( "/" )[-1].split( "." )[0]
+    print()
+    print( "+-" + "-"*len(title) + "-+" )
+    print( "| {} |".format(title) )
+    print( "+-" + "-"*len(title) + "-+" )
+    print()
+    knotCount = 0
+    longestName = None
+    longestTime = 0
+    start = default_timer()
     with open( filename, "r" ) as table:
         headings = table.readline().rstrip().split( "," )
         nameCol = headings.index( "name" )
@@ -73,27 +74,33 @@ def decomposeAllInTable( filename, verbose=True ):
             knot = Link.fromKnotSig( entries[sigCol] )
             if knot.countComponents() != 1:
                 continue
-            if verbose:
-                name = "Knot #{}: {}".format(
-                        knotCount, entries[nameCol] )
-                print(name)
-                print( "-"*len(name) )
-                knotCount += 1
-            primes = decompose( knot, verbose )
-            if verbose:
-                if len(primes) == 0:
-                    print( "Unknot!" )
-                elif len(primes) == 1:
-                    print( "Found 1 prime:" )
-                else:
-                    print( "Found {} primes:".format( len(primes) ) )
-                for i, loop in enumerate(primes):
-                    print( "    Drilled iso sig for prime #{}: {}".format(
-                        i, loop.drill().isoSig() ) )
-                print()
+            name = "Knot #{}: {}".format(
+                    knotCount, entries[nameCol] )
+            print(name)
+            print( "-"*len(name) )
+            knotCount += 1
+            tracker = DecompositionTracker()
+            primes = decompose( knot, tracker )
+            if len(primes) == 0:
+                print( "Unknot!" )
+            elif len(primes) == 1:
+                print( "Found 1 prime:" )
+            else:
+                print( "Found {} primes:".format( len(primes) ) )
+            for i, loop in enumerate(primes):
+                print( "    Drilled iso sig for prime #{}: {}".format(
+                    i, loop.drill().isoSig() ) )
+            if tracker.elapsed() > longestTime:
+                longestName = name
+                longestTime = tracker.elapsed()
+            print()
     print( "="*32 )
     print( "Total knots: {}.".format(knotCount) )
     print( "Total time: {:.6f}.".format( default_timer() - start ) )
+    if knotCount:
+        print( "Longest:\n    {}. {:.6f}.".format(
+            longestName, longestTime ) )
+    print()
     return
 
 
