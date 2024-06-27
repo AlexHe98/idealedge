293e31af139edcf007a2e786ab83213662033b1c
alex-he <mr.a.he98@gmail.com>
2024-06-27 20:50:24 +1000

    Experiment with decomposing knots given by composing large primes.

--- compose.py
+++ compose.py
@@ -4,13 +4,19 @@ Generate random composite knots by sampling from knot tables.
 from sys import argv
 from regina import *
 from sample import sample
+from decomposeknot import decompose, DecompositionTracker
 
 
-def generateComposite( numKnots, numSummands, *filenames ):
+def generateComposites( numKnots, numSummands, *filenames ):
     """
     Generates the given number of knots, each given by composing the given
     number of summands, by randomly sampling summands from the knot table(s)
     in the given file(s).
+
+    Each given file should be a CSV file that includes (at least) the
+    following data:
+    --> a column of knot names under the heading "name"; and
+    --> a column of knot signatures under the heading "knot_sig".
     """
     knotNamesAndSigs = sample( numKnots * numSummands, *filenames )
     numSampled = len(knotNamesAndSigs)
@@ -35,8 +41,72 @@ def generateComposite( numKnots, numSummands, *filenames ):
     return composites
 
 
+def decomposeComposites( numKnots, numSummands, *filenames ):
+    """
+    Generates the given number of knots, each given by composing the given
+    number of summands, and then decomposes all the generated knots.
+
+    The summands are sampled from the knot table(s) in the given files(s).
+    Each such file should be a CSV file that includes (at least) the
+    following data:
+    --> a column of knot names under the heading "name"; and
+    --> a column of knot signatures under the heading "knot_sig".
+
+    The routine prints the results of each decomposition to standard output.
+    """
+    title = "Knots given by composing {} summands".format(numSummands)
+    print()
+    print( "+-" + "-"*len(title) + "-+" )
+    print( "| {} |".format(title) )
+    print( "+-" + "-"*len(title) + "-+" )
+    print()
+    print( "Summands sampled from the following data sets:" )
+    for filename in filenames:
+        print( "    {}".format(
+            filename.split( "/" )[-1].split( "." )[0] ) )
+    print()
+    data = []
+    totalTime = 0
+    knots = generateComposites( numKnots, numSummands, *filenames )
+    for name, knot in knots:
+        print(name)
+        print( "-"*len(name) )
+        tracker = DecompositionTracker(True)
+        primes = decompose( knot, tracker )
+        if len(primes) == 0:
+            print( "Unknot!" )
+        elif len(primes) == 1:
+            print( "Found 1 prime:" )
+        else:
+            print( "Found {} primes:".format( len(primes) ) )
+        for i, loop in enumerate(primes):
+            print( "    Drilled iso sig for prime #{}: {}".format(
+                i, loop.drill().isoSig() ) )
+
+        # Store data for post-processing.
+        data.append( ( name, tracker.elapsed() ) )
+        totalTime += tracker.elapsed()
+        print()
+    print( "="*32 )
+    print( "Total knots: {}.".format(numKnots) )
+    print( "Total time: {:.6f}.".format(totalTime) )
+    if knots:
+        slowCoefficient = 2
+        average = totalTime / numKnots
+        print( "Cases slower than {} times the average:".format(
+            slowCoefficient ) )
+        noSlowCases = True
+        for name, time in data:
+            if time > slowCoefficient * average:
+                noSlowCases = False
+                print( "    Name: {}. Time: {:.6f}.".format( name, time ) )
+        if noSlowCases:
+            print( "    (None)" )
+    print()
+    return
+
+
 if __name__ == "__main__":
     numKnots = int( argv[1] )
     numSummands = int( argv[2] )
-    for name, _ in generateComposite( numKnots, numSummands, *argv[3:] ):
-        print(name)
+    decomposeComposites( numKnots, numSummands, *argv[3:] )
