6b03d5c05665bf4857ace65f6be1357bbdce8a4e
alex-he <mr.a.he98@gmail.com>
2024-11-05 23:22:24 -0600

    Documentation for the 3-sphere edge experiment code.

--- s3edges.py
+++ s3edges.py
@@ -9,6 +9,14 @@ from experiment import runDecompositionExperiment
 
 def decomposeS3Edges(dataset):
     """
+    Decomposes the knots represented by the edges of the one-vertex 3-spheres
+    in the given dataset.
+
+    This routine uses the readS3Edges routine to extract knots from the given
+    dataset, so the format of the dataset must adhere to the specifications
+    stated in the documentation for readS3Edges().
+
+    This routine prints the results of each decomposition to standard output.
     """
     print()
     print( "+-------------------------------+" )
@@ -24,6 +32,17 @@ def decomposeS3Edges(dataset):
 
 def readS3Edges(dataset):
     """
+    Reads edges of one-vertex 3-spheres from the given dataset.
+
+    The given dataset should be the name of a text file with one isomorphism
+    signature per line. Each listed isomorphism signature should correspond
+    to a one-vertex triangulation of the 3-sphere.
+
+    For each isomorphism signature S in the given dataset, this routine
+    iterates through all the edges of the corresponding triangulation T. For
+    each such edge E, this routine yields a pair (D, E), where D is a string
+    that gives a description of E. Specifically, D will be of the form
+    "{S}, edge {i}", where i is the index of E in T.
     """
     with open( dataset, "r" ) as lines:
         for sigline in lines:
