9525cfa3e37097f383a5162bd113935349efa8a1
alex-he <mr.a.he98@gmail.com>
2025-02-15 21:59:25 -0600

    Update Regina names and make output from decomposeknotsig.py more helpful.

--- decomposeknotsig.py
+++ decomposeknotsig.py
@@ -9,5 +9,13 @@ from decomposeknot import decompose
 if __name__ == "__main__":
     # Run decompose() with the verbose option.
     print()
-    decompose( Link.fromKnotSig( argv[1] ), True )
-    print()
+    primes = decompose( Link.fromKnotSig( argv[1] ), True )
+    if len(primes) == 0:
+        print( "Unknot!" )
+    elif len(primes) == 1:
+        print( "Found 1 prime:" )
+    else:
+        print( "Found {} primes:".format( len(primes) ) )
+    for i, loop in enumerate(primes):
+        print( "    Drilled iso sig for prime #{}: {}".format(
+            i, loop.drill().isoSig() ) )
