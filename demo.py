b0103bc66390f5ea482614b10f8977704d14ddd4
alex-he <mr.a.he98@gmail.com>
2025-03-05 22:25:34 -0600

    Script for demonstrating the algorithm with a single hard diagram of a composite knot.

---
+++ demo.py
@@ -0,0 +1,30 @@
+"""
+Demo with hard diagrams of composite knots.
+"""
+from sys import argv
+import snappy
+from regina import *
+from hardknot import randomHardComposite
+from decomposeknot import decompose
+
+
+if __name__ == "__main__":
+    try:
+        sig = argv[1]
+    except IndexError:
+        # If no knot signature is provided, then try to randomly generate a
+        # hard diagram of a composite knot.
+        _, snappyKnot = randomHardComposite(2)
+        pd = snappyKnot.PD_code( min_strand_index=1 )
+        knot = Link.fromPD(pd)
+    else:
+        knot = Link.fromKnotSig(sig)
+        snappyKnot = snappy.Link( knot.pdData() )
+
+    # View the knot diagram using the PLink viewer.
+    #NOTE This doesn't seem to work on all machines.
+    snappyKnot.view().window.mainloop()
+
+    # Now try to decompose the knot (in verbose mode).
+    print()
+    decompose( knot, True )
