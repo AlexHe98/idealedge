da99f8632917263bc02860ec23cc24c78bb2c4c3
alex-he <mr.a.he98@gmail.com>
2025-06-11 16:32:54 +1000

    Traverse wedge cells (part 2 of 2).

--- wedge.py
+++ wedge.py
@@ -1,6 +1,7 @@
 """
 Traverse wedge cells to detect lost L(3,1) components.
 """
+from sys import argv
 from regina import *
 from parallel import tetQuadType
 
@@ -9,7 +10,6 @@ def wedgeLoops(surf):
     """
     Detects loops of wedge cells induced by the given normal surface.
     """
-    #TODO Decide on returned data structure.
     tri = surf.triangulation()
 
     # Find all wedge cells.
@@ -26,39 +26,44 @@ def wedgeLoops(surf):
         # at the front in the figure below, then the vertex numbers of tet
         # will be as shown in the figure.
         #
-        #                  eOrder[i][0]
-        #                        *
-        #                       /|\
-        #                      / | \
-        #                     /__|__\
-        #                    /|  |  |\
-        #                   / |  |  | \
-        #   eOrder[1-i][0] *--|--|--|--* eOrder[1-i][1]
-        #                   \ |  |  | /
-        #                    \|__|__|/
-        #                     \  |  /
-        #                      \ | /
-        #                       \|/
-        #                        *
-        #                  eOrder[i][1]
+        #               eOrder[1-i][0]
+        #                      *
+        #                     /|\
+        #                    / | \
+        #                   /__|__\
+        #                  /|  |  |\
+        #                 / |  |  | \
+        #   eOrder[i][0] *--|--|--|--* eOrder[i][1]
+        #                 \ |  |  | /
+        #                  \|__|__|/
+        #                   \  |  /
+        #                    \ | /
+        #                     \|/
+        #                      *
+        #               eOrder[1-i][1]
         #
         eOrder = [ Edge3.ordering(quadType), Edge3.ordering(5-quadType) ]
         wedgeAdjacencies[teti] = dict()
         for i in range(2):
             wedge = ( teti, eOrder[i][0], eOrder[i][1] )
-            wedgePerms[wedge] = eOrder[i] * Perm4(2,3) * eOrder[i].inverse()
+            wedgePerms[wedge] = eOrder[i] * Perm4(0,1) * eOrder[i].inverse()
             for ii in range(2):
                 # Record adjacency of faces across wedge cells; we need this
                 # information to traverse across wedge cells.
-                wedgeAdjacencies[teti][eOrder[1-i][ii]] =\
-                        ( eOrder[1-i][1-ii], wedge )
+                wedgeAdjacencies[teti][eOrder[i][ii]] =\
+                        ( eOrder[i][1-ii], wedge )
 
     # Traverse all wedge cells.
+    #TODO Decide on returned data structure.
+    #   Currently using a set of pairs of the form (r,t), where r is a
+    #   representative wedge cell, and t is True if and only if the loop has
+    #   a twist.
+    loops = set()
     while wedgePerms:
-        startWedge, startPerm = wedgePerms.popitem()
+        startWedge, endPerm = wedgePerms.popitem()
         startTeti, currentFace, endFace = startWedge
         currentTet = tri.tetrahedron(startTeti)
-        currentPerm = Perm4()
+        vertPerm = Perm4()
 
         # Traverse until one of the following occurs:
         #   --> We return to the start (in which case we have found a loop of
@@ -73,18 +78,46 @@ def wedgeLoops(surf):
             adjTeti = adjTet.index()
             adjFace = currentTet.adjacentFace(currentFace)
             adjGluing = currentTet.adjacentGluing(currentFace)
-            currentPerm = adjGluing * currentPerm
+            vertPerm = adjGluing * vertPerm
 
             # Have we reached a new wedge cell?
             if adjTeti not in wedgeAdjacencies:
-                # No wedge cells in adjTet.
-                #TODO
+                # No wedge cells in adjTet, and hence we are not traversing
+                # a loop of wedge cells.
                 break
-            nextFace, adjWedge = wedgeAdjacencies[adjTet.index()][adjFace]
-            #TODO
+            currentFace, adjWedge = wedgeAdjacencies[adjTet.index()][adjFace]
 
-            # Traverse across adjacent wedge cell.
-            #TODO
-            pass
-    #TODO
-    raise NotImplementedError()
+            # Have we already previously traversed the new wedge cell? If so,
+            # then either:
+            #   --> we have returned to the start of a loop of wedge cells;
+            #       or
+            #   --> we are not traversing a loop of wedge cells.
+            if adjWedge not in wedgePerms:
+                # If we are back to the start of a loop, then make a note of
+                # whether or not this loop has a twist.
+                if adjWedge == startWedge:
+                    vertPerm = endPerm * vertPerm
+                    loops.add( ( startWedge, not vertPerm.isIdentity() ) )
+
+                # Regardless of whether or not we had a loop, there is no
+                # further traversal we can do.
+                break
+
+            # Traverse across the new wedge cell.
+            # No need to set currentFace since that was done earlier.
+            vertPerm = wedgePerms[adjWedge] * vertPerm
+            currentTet = adjTet
+
+    # All done!
+    return loops
+
+
+if __name__ == "__main__":
+    sig = argv[1]
+    num = int( argv[2] )
+    tri = Triangulation3.fromIsoSig(sig)
+    qvsurfs = NormalSurfaces( tri, NormalCoords.NS_QUAD )
+    surf = qvsurfs[num]
+
+    #TODO TEST
+    print( wedgeLoops(surf) )
