9d441921208b5ced8bb031e176dccf142d854c63
alex-he <mr.a.he98@gmail.com>
2025-12-04 11:20:36 +1000

    Overhaul blueprints (part 3).

--- blueprint.py
+++ blueprint.py
@@ -16,32 +16,30 @@ def triangulationBlueprint(tri):
     """
     Returns a picklable blueprint for the given 3-manifold triangulation tri.
 
-    In detail, this routine returns a tuple (S,T,F), where:
-    --> S is the isomorphism signature for tri;
-    --> T is a list such that T[i] gives the index of the tetrahedron in
-        newTri := Triangulation3.fromIsoSig(S) that corresponds to
-        tetrahedron i of tri; and
-    --> F is a list such that F[i] is length-4 list with F[i][j] being
-        the vertex number of newTri.tetrahedron( T[i] ) that corresponds
-        to vertex j of tri.tetrahedron(i).
-    The returned blueprint can be used, via the reconstructTriangulation()
+    In detail, this routine returns a pair (S,G), where:
+    --> S is the size (i.e., the number of tetrahedra) of tri.
+    --> G is a list such that each entry is of the form [i,f,j,perm],
+        and describes a gluing of tri as follows:
+        --- i is a tetrahedron index of tri;
+        --- f is a face number of tetrahedron i;
+        --- j is the index of the tetrahedron adjacent to tetrahedron i along
+            face f; and
+        --- perm is a list describing the gluing along face f of tetrahedron
+            i, with perm[v] giving, for each v in {0,1,2,3}, the vertex number
+            of tetrahedron j corresponding to vertex v of tetrahedron i.
+    This is essentially the data required to construct a triangulation using
+    the Triangulation3.fromGluings() routine, except that the gluing
+    permutations are given in a picklable format.
+
+    The returned blueprint can be used, via the reconstructionTriangulation()
     routine, to build a clone of the given triangulation. Specifically, the
     following command would return a clone of tri:
         reconstructTriangulation( *triangulationBlueprint(tri) )
     """
-    sig, isom = tri.isoSigDetail()
-
-    # Convert the isomorphism into the lists T and F.
-    tetImages = []
-    facePerms = []
-    for i in range( tri.size() ):
-        tetImages.append( isom.simpImage(i) )
-        facePerms.append(
-                [ isom.facetPerm(i)[j] for j in range(4) ] )
-    return ( sig, tetImages, facePerms )
+    return ( tri.size(), _gluings(tri) )
 
 
-def reconstructTriangulation( sig, tetImages, facePerms ):
+def reconstructTriangulation( size, gluings ):
     """
     Returns a triangulation reconstructed from the given data.
 
@@ -51,13 +49,30 @@ def reconstructTriangulation( sig, tetImages, facePerms ):
     command would return a clone of a 3-manifold triangulation tri:
         reconstructTriangulation( *triangulationBlueprint(tri) )
     """
-    tri = Triangulation3.fromIsoSig(sig)
+    # Convert the picklable gluings into a description that we can pass to
+    # Triangulation3.fromGluings().
+    use = []
+    for g in gluings:
+        use.append( [ g[0], g[1], g[2], Perm4(*g[3]) ] )
+    return Triangulation3.fromGluings( size, use )
 
-    # Adjust tri to use the original labelling (instead of the canonical
-    # labelling given by fromIsoSig()).
-    isom = Isomorphism3( tri.size() )
-    for i in range( tri.size() ):
-        isom.setSimpImage( i, tetImages[i] )
-        isom.setFacePerm( i, Perm4( *facePerms[i] ) )
-    tri = isom.inverse()(tri)
-    return tri
+
+def _gluings(tri):
+    """
+    Returns a picklable description of the gluings used to construct tri.
+    """
+    # We could get this information by parsing the output of tri.source(),
+    # but it seems better to just compute this for ourselves.
+    ans = []
+    for face in tri.triangles():
+        # By iterating through the triangles, we ensure that we only encode
+        # each gluing once.
+        myTet = face.front().tetrahedron()
+        myFace = face.front().face()
+        yourTet = myTet.adjacentTetrahedron(myFace)
+        if yourTet is None:
+            continue
+        gluing = myTet.adjacentGluing(myFace)
+        ans.append( [ myTet.index(), myFace, yourTet.index(),
+                     [ gluing[i] for i in range(4) ] ] )
+    return ans
