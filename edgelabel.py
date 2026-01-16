267e73754acda46c38c3edf2c2b044d7609f7269
alex-he <mr.a.he98@gmail.com>
2026-01-16 23:03:23 +1000

    Revamp moves to track orientation (part 27): new EdgeLabelling class (part 2).

--- edgelabel.py
+++ edgelabel.py
@@ -56,18 +56,128 @@ class EdgeLabelling:
         of this merge, it is useful to allow two edge indices to map to
         EdgeEmbedding3 objects corresponding to the same edge.
     """
-    def __init__( self, tri ):
+    def __init__( self, tri, labelling=None ):
         """
-        Initialises the default edge labelling for the given triangulation.
+        Initialises an edge labelling for the given triangulation.
 
-        The default coincides with the underlying labelling as follows:
+        If no labelling data is supplied, then the default behaviour is to
+        assign a labelling that coincides, in the following sense, with the
+        underlying labelling given by tri:
         --> For every edge, the default labelling assigns the same integer
             index as the underlying labelling.
         --> The default EdgeEmbedding3 object associated to each edge e is
             just e.front().
+
+        Otherwise, the labelling data should be a dictionary whose keys are
+        integers and whose values are EdgeEmbedding3 objects corresponding to
+        edges in tri.
         """
+        if labelling is None:
+            labelling = { e.index(): e.front() for e in tri.edge() }
         self._tri = tri
-        self._labelling = { e.index(): e.front() for e in tri.edges() }
+        self._labelling = labelling
         return
+
+    def triangulation(self):
+        """
+        Returns the underlying triangulation.
+        """
+        return self._tri
+
+    def cloneLabelling(self):
+        """
+        Returns a clone of this labelling on the same underlying
+        triangulation.
+        """
+        clonedLabelling = { i: EdgeEmbedding3(emb)
+                           for i, emb in self._labelling.items() }
+        return EdgeLabelling( self._tri, clonedLabelling )
+
+    def clone(self):
+        """
+        Returns a clone of this labelling on a clone of the underlying
+        triangulation.
+        """
+        clonedTri = Triangulation3(self._tri)
+        clonedLabelling = dict()
+        for i, emb in self._labelling.items():
+            tet = clonedTri.tetrahedron( emb.tetrahedron().index() )
+            clonedLabelling[i] = EdgeEmbedding3( tet, emb.vertices() )
+        return EdgeLabelling( clonedTri, clonedLabelling )
+
+    def __len__(self):
+        """
+        Returns the number of edges tracked by this labelling.
+        """
+        return len(self._labelling)
+
+    def __getitem__( self, index ):
+        """
+        Returns the EdgeEmbedding3 object at the given index, or None if no
+        such object is assigned.
+        """
+        return self.get(index)
+
+    def get( self, index ):
+        """
+        Returns the EdgeEmbedding3 object at the given index, or None if no
+        such object is assigned.
+        """
+        return self._labelling.get( index, None )
+
+    def __setitem__( self, index, emb ):
+        """
+        Assigns the given EdgeEmbedding3 object emb to the given index.
+        """
+        self._labelling[index] = emb
+        return
+
+    def untrack( self, index ):
+        """
+        Stops tracking the edge at the given index.
+
+        Returns True if the index status changed from tracked to untracked,
+        and False if the index was already untracked to begin with.
+        """
+        # Status changed if and only if pop() returns an actual EdgeEmbedding3
+        # object (instead of None).
+        return ( self._labelling.pop( index, None ) is not None )
+
+    def __contains__( self, index ):
+        """
+        Is the given index tracking an edge?
+        """
+        return ( index in self._labelling )
+
+    def isTracking( self, index ):
+        """
+        Is the given index tracking an edge?
+        """
+        return ( index in self )
+
+    def __iter__(self):
+        """
+        Returns an iterator over the tracked indices.
+        """
+        return iter(self._labelling)
+
+    def items(self):
+        """
+        Returns a view of the (index, embedding) pairs of this labelling.
+        """
+        return self._labelling.items()
+
+    def trackedIndices(self):
+        """
+        Returns a view of the indices tracked by this labelling.
+        """
+        return self._labelling.keys()
+
+    def edgeEmbeddings(self):
+        """
+        Returns a view of the EdgeEmbedding3 objects tracked by this
+        labelling.
+        """
+        return self._labelling.values()
 
     #TODO
