bae7c68b862776836955968ce99112f1b57f09e1
alex-he <mr.a.he98@gmail.com>
2026-01-19 17:28:46 +1000

    Separate source from tests.

---
+++ tests/sfstri.py
@@ -0,0 +1,194 @@
+"""
+Test suite for triangulations of orientable Seifert fibre spaces.
+"""
+from regina import *
+from surftri import surface
+from sfstri import orientableSFS, OrientableBundle, TriPrism
+from sys import argv
+from tests.aux import parseTestNames, doTest, allTestsPassedMessage
+
+
+if __name__ == "__main__":
+    availableTests = [ "sfs",
+                      "bundle",
+                      "prism" ]
+    testNames = parseTestNames( argv[1:], availableTests )
+
+    # Test orientableSFS() routine.
+    if "sfs" in testNames:
+        print( "+-------------------------------------------------------+" )
+        print( "| orientableSFS( baseSignedGenus, boundaries, *fibres ) |" )
+        print( "+-------------------------------------------------------+" )
+        testFibres = [
+                [],
+                [ (5,2) ],
+                [ (3,-1), (4,1) ],
+                [ (2,1), (5,1), (5,-2) ],
+                [ (3,1), (7,-2), (7,2), (7,-3) ] ]
+        #NOTE Regina is not always consistent with how it chooses from the
+        #   many alternative names that these manifolds have.
+        expectedNames = [
+                "RP3 # RP3",
+                "SFS [S2: (2,1) (2,1) (2,3)]",
+                "SFS [RP2/n2: (3,1) (4,-1)]",
+                "SFS [RP2/n2: (2,1) (5,1) (5,-2)]",
+                "SFS [RP2/n2: (3,1) (7,2) (7,4) (7,-9)]",
+                "M/n2 x~ S1",
+                "SFS [M/n2: (5,3)]",
+                "SFS [M/n2: (3,1) (4,3)]",
+                "SFS [M/n2: (2,1) (5,2) (5,-1)]",
+                "SFS [M/n2: (3,2) (7,2) (7,3) (7,-2)]",
+                "S2 x S1",
+                "RP3",
+                "S3",
+                "SFS [S2: (2,1) (5,1) (5,-2)]",
+                "SFS [S2: (3,1) (7,2) (7,4) (7,-9)]",
+                "B2 x S1",
+                "B2 x S1",
+                "SFS [D: (3,1) (4,-1)]",
+                "SFS [D: (2,1) (5,1) (5,-2)]",
+                "SFS [D: (3,1) (7,2) (7,4) (7,-9)]",
+                "T x S1",
+                "SFS [T: (5,2)]",
+                "SFS [T: (3,1) (4,-1)]",
+                "SFS [T: (2,1) (5,1) (5,-2)]",
+                "SFS [T: (3,1) (7,2) (7,4) (7,-9)]",
+                "Or, g=1 + 1 puncture x S1",
+                "SFS [Or, g=1 + 1 puncture: (5,3)]",
+                "SFS [Or, g=1 + 1 puncture: (3,1) (4,3)]",
+                "SFS [Or, g=1 + 1 puncture: (2,1) (5,2) (5,-1)]",
+                "SFS [Or, g=1 + 1 puncture: (3,2) (7,2) (7,3) (7,-2)]" ]
+        nameIndex = -1
+        for genus in range( -1, 2 ):
+            for boundaries in range(2):
+                for fibres in testFibres:
+                    print( "g={}, b={}, fibres={}.".format(
+                        genus, boundaries, fibres ) )
+                    nameIndex += 1
+                    expectedName = expectedNames[nameIndex]
+                    sfs = orientableSFS( genus, boundaries, *fibres )
+                    doTest( "Oriented?", True, sfs.isOriented() )
+                    if expectedName == "RP3 # RP3":
+                        #NOTE StandardTriangulation doesn't recognise the
+                        #   minimal triangulation of RP3 # RP3.
+                        print( "Skipped recognition of RP3 # RP3." )
+                    else:
+                        actual = StandardTriangulation.recognise(sfs)
+                        if actual.manifold().structure():
+                            actualName = actual.manifold().structure()
+                        else:
+                            actualName = actual.manifold().name()
+                        doTest( "Manifold?",
+                               expectedName, actualName )
+                    print()
+
+    # Test OrientableBundle class.
+    if "bundle" in testNames:
+        print( "+------------------------+" )
+        print( "| OrientableBundle class |" )
+        print( "+------------------------+" )
+
+        # Check that we get oriented triangulations of the correct
+        # 3-manifolds.
+        #NOTE Regina is not always consistent with how it chooses from the
+        #   many alternative names that these manifolds have.
+        expectedNames = [
+                "KB/n2 x~ S1",
+                "Non-or, g=2 + 1 puncture/n2 x~ S1",
+                "Non-or, g=2 + 2 punctures/n2 x~ S1",
+                "RP3 # RP3",
+                "M/n2 x~ S1",
+                "Non-or, g=1 + 2 punctures/n2 x~ S1",
+                "S2 x S1",
+                "SFS [D: (1,1)]",
+                "SFS [A: (1,1)]",
+                "T x S1",
+                "Or, g=1 + 1 puncture x S1",
+                "Or, g=1 + 2 punctures x S1",
+                "Or, g=2 x S1",
+                "Or, g=2 + 1 puncture x S1",
+                "Or, g=2 + 2 punctures x S1" ]
+        nameIndex = -1
+        for genus in range( -2, 3 ):
+            for boundaries in range(3):
+                print( "g={}, b={}.".format( genus, boundaries ) )
+                nameIndex += 1
+                expectedName = expectedNames[nameIndex]
+                base = surface( genus, boundaries )
+
+                # Circle bundle.
+                bundle = OrientableBundle( base, True )
+                tri = bundle.triangulation()
+                doTest( "Oriented?", True, tri.isOriented() )
+                actual = StandardTriangulation.recognise(tri).manifold()
+                doTest( "Manifold?", expectedName, actual.name() )
+                for edge in base.edges():
+                    if not edge.isBoundary():
+                        continue
+                    faceIndex = edge.front().triangle().index()
+                    prism = bundle.triPrism(faceIndex)
+                    square = edge.front().edge()
+                    doTest(
+                            "Square {}({}) glued?".format(
+                                faceIndex, square ),
+                            False, prism.isSquareGlued(square) )
+                print()
+
+    # Test TriPrism class.
+    if "prism" in testNames:
+        print( "+----------------+" )
+        print( "| TriPrism class |" )
+        print( "+----------------+" )
+
+        def _testTri( tri, isSolidTorus ):
+            """
+            Test basic properties of the triangulation.
+            """
+            doTest( "Solid torus?", isSolidTorus, tri.isSolidTorus() )
+            doTest( "Ball?", not isSolidTorus, tri.isBall() )
+            doTest( "Oriented?", True, tri.isOriented() )
+            return
+
+        def _testSlopeSign( prism, s, tri, isSolidTorus ):
+            """
+            Test that the slope-sign constraint holds both before and after
+            flipping square s of the given prism.
+            """
+            # Before flipping.
+            oldSlope = prism.squareSlope(s)
+            print( "Old slope of square {}: {}.".format(
+                s, oldSlope ) )
+            for t in range(2):
+                doTest( "Slope-sign constraint for triangle {}.".format(t),
+                       -1, oldSlope * prism.squareRoles(s,t).sign() )
+
+            # After flipping.
+            newSlope = prism.flipSlope(s)
+            doTest( "Flip slope of square {}.".format(s),
+                    -oldSlope, newSlope )
+            doTest( "New slope of square {}.".format(s),
+                    -oldSlope, prism.squareSlope(s) )
+            _testTri( tri, isSolidTorus )
+            for t in range(2):
+                doTest( "Slope-sign constraint for triangle {}.".format(t),
+                        -1, newSlope * prism.squareRoles(s,t).sign() )
+            print()
+            return
+
+        # Test both solid-torus and non-solid-torus constructions.
+        for isSolidTorus in ( True, False ):
+            tri = Triangulation3()
+            prism = TriPrism( tri, isSolidTorus )
+            print( "------------------------------------------------" )
+            _testTri( tri, isSolidTorus )
+            print( "------------------------------------------------" )
+            print()
+
+            # Flip everything one by one, and then unflip, and test that the
+            # slope-sign constraint is preserved all the way through.
+            for _ in range(2):
+                for s in range(3):
+                    _testSlopeSign( prism, s, tri, isSolidTorus )
+
+    # If we make it here, then all tests passed.
+    allTestsPassedMessage(testNames)
---
+++ tests/surftri.py
@@ -0,0 +1,61 @@
+"""
+Test suite for minimal triangulations of surfaces.
+"""
+from regina import *
+from surftri import orientable, nonOrientable
+from sys import argv
+from tests.aux import parseTestNames, doTest, allTestsPassedMessage
+
+
+if __name__ == "__main__":
+    availableTests = [ "orbl",
+                      "norbl" ]
+    testNames = parseTestNames( argv[1:], availableTests )
+
+    # Test orientable() routine.
+    if "orbl" in testNames:
+        print( "+---------------------------------+" )
+        print( "| orientable( genus, boundaries ) |" )
+        print( "+---------------------------------+" )
+        for genus in range(9):
+            for boundaries in range(9):
+                print( "g={}, b={}".format( genus, boundaries ) )
+                surf = orientable( genus, boundaries )
+                if boundaries == 0:
+                    eulerBound = 4*genus - 2
+                    expectedSize = max( 2, eulerBound )
+                else:
+                    eulerBound = 4*genus + 3*boundaries - 4
+                    expectedSize = max( 1, eulerBound )
+                doTest( "Size.", expectedSize, surf.size() )
+                expectedEuler = 2 - 2*genus - boundaries
+                doTest( "Euler.", expectedEuler, surf.eulerChar() )
+                doTest( "Boundary components.",
+                       boundaries, surf.countBoundaryComponents() )
+                doTest( "Oriented?", True, surf.isOriented() )
+                print()
+
+    # Test nonOrientable() routine.
+    if "norbl" in testNames:
+        print( "+------------------------------------+" )
+        print( "| nonOrientable( genus, boundaries ) |" )
+        print( "+------------------------------------+" )
+        for genus in range( 1, 9 ):
+            for boundaries in range(9):
+                print( "g={}, b={}".format( genus, boundaries ) )
+                surf = nonOrientable( genus, boundaries )
+                if boundaries == 0:
+                    eulerBound = 2*genus - 2
+                    expectedSize = max( 2, eulerBound )
+                else:
+                    expectedSize = 2*genus + 3*boundaries - 4
+                doTest( "Size.", expectedSize, surf.size() )
+                expectedEuler = 2 - genus - boundaries
+                doTest( "Euler.", expectedEuler, surf.eulerChar() )
+                doTest( "Boundary components.",
+                       boundaries, surf.countBoundaryComponents() )
+                doTest( "Orientable?", False, surf.isOrientable() )
+                print()
+
+    # If we make it here, then all tests passed.
+    allTestsPassedMessage(testNames)
