db6a2bfacc22f498174d821eee70ceb397002ac8
alex-he <mr.a.he98@gmail.com>
2026-01-20 01:30:51 +1000

    More tidying up of tests (part 4).

--- tests/surftri.py
+++ tests/surftri.py
@@ -4,7 +4,56 @@ Test suite for minimal triangulations of surfaces.
 from regina import *
 from surftri import orientable, nonOrientable
 from sys import argv
-from tests.aux import parseTestNames, doTest, allTestsPassedMessage
+from tests.aux import parseTestNames, allTestsPassedMessage
+from tests.aux import doTest, runNamedTestSuite
+
+
+def testOrientable():
+    count = 0
+    for genus in range(9):
+        for boundaries in range(9):
+            count += 1
+            description = "g={}, b={}.".format( genus, boundaries )
+            surf = orientable( genus, boundaries )
+            if boundaries == 0:
+                eulerBound = 4*genus - 2
+                expectedSize = max( 2, eulerBound )
+            else:
+                eulerBound = 4*genus + 3*boundaries - 4
+                expectedSize = max( 1, eulerBound )
+            doTest( description + " Size.",
+                   expectedSize, surf.size() )
+            expectedEuler = 2 - 2*genus - boundaries
+            doTest( description + " Euler.",
+                   expectedEuler, surf.eulerChar() )
+            doTest( description + " Boundary components.",
+                   boundaries, surf.countBoundaryComponents() )
+            doTest( description + " Oriented?", True, surf.isOriented() )
+    return count
+
+
+def testNonOrientable():
+    count = 0
+    for genus in range( 1, 9 ):
+        for boundaries in range(9):
+            count += 1
+            description = "g={}, b={}.".format( genus, boundaries )
+            surf = nonOrientable( genus, boundaries )
+            if boundaries == 0:
+                eulerBound = 2*genus - 2
+                expectedSize = max( 2, eulerBound )
+            else:
+                expectedSize = 2*genus + 3*boundaries - 4
+            doTest( description + " Size.",
+                   expectedSize, surf.size() )
+            expectedEuler = 2 - genus - boundaries
+            doTest( description + " Euler.",
+                   expectedEuler, surf.eulerChar() )
+            doTest( description + " Boundary components.",
+                   boundaries, surf.countBoundaryComponents() )
+            doTest( description + " Orientable?",
+                   False, surf.isOrientable() )
+    return count
 
 
 if __name__ == "__main__":
@@ -14,54 +63,13 @@ if __name__ == "__main__":
 
     # Test orientable() routine.
     if "orbl" in testNames:
-        print( "+---------------------------------+" )
-        print( "| orientable( genus, boundaries ) |" )
-        print( "+---------------------------------+" )
-        for genus in range(9):
-            for boundaries in range(9):
-                print( "g={}, b={}".format( genus, boundaries ) )
-                surf = orientable( genus, boundaries )
-                if boundaries == 0:
-                    eulerBound = 4*genus - 2
-                    expectedSize = max( 2, eulerBound )
-                else:
-                    eulerBound = 4*genus + 3*boundaries - 4
-                    expectedSize = max( 1, eulerBound )
-                doTest( "Size.", expectedSize, surf.size() )
-                expectedEuler = 2 - 2*genus - boundaries
-                doTest( "Euler.", expectedEuler, surf.eulerChar() )
-                doTest( "Boundary components.",
-                       boundaries, surf.countBoundaryComponents() )
-                doTest( "Oriented?", True, surf.isOriented() )
-
-        # End of orientable() test.
-        print()
-        pass
+        longName = "orientable( genus, boundaries )"
+        runNamedTestSuite( longName, testOrientable )
 
     # Test nonOrientable() routine.
     if "norbl" in testNames:
-        print( "+------------------------------------+" )
-        print( "| nonOrientable( genus, boundaries ) |" )
-        print( "+------------------------------------+" )
-        for genus in range( 1, 9 ):
-            for boundaries in range(9):
-                print( "g={}, b={}".format( genus, boundaries ) )
-                surf = nonOrientable( genus, boundaries )
-                if boundaries == 0:
-                    eulerBound = 2*genus - 2
-                    expectedSize = max( 2, eulerBound )
-                else:
-                    expectedSize = 2*genus + 3*boundaries - 4
-                doTest( "Size.", expectedSize, surf.size() )
-                expectedEuler = 2 - genus - boundaries
-                doTest( "Euler.", expectedEuler, surf.eulerChar() )
-                doTest( "Boundary components.",
-                       boundaries, surf.countBoundaryComponents() )
-                doTest( "Orientable?", False, surf.isOrientable() )
-
-        # End of nonOrientable() test.
-        print()
-        pass
+        longName = "nonOrientable( genus, boundaries )"
+        runNamedTestSuite( longName, testNonOrientable )
 
     # If we make it here, then all tests passed.
     allTestsPassedMessage(testNames)
