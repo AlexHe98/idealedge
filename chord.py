4e84d91bc16d1b9a2d405f3d3fb132fcef9829f7
alex-he <mr.a.he98@gmail.com>
2026-07-23 00:48:09 +1000

    NormalChord (part 4).

--- chord.py
+++ chord.py
@@ -1,24 +1,113 @@
 """
 Implements various types of embedded arcs in triangulations.
 """
+from abc import ABC, abstractmethod
 
 
-class NormalChord:
+class NormalChord(ABC):
     """
     A "chord" for a normal surface.
 
-    This is an embedded arc whose endpoints lie on a normal surface S, and
-    whose interior lies in the complement of S.
+    Here, a "chord" means an embedded arc whose endpoints lie on a normal
+    surface S, and whose interior lies in the complement of S. This abstract
+    base class does not actually implement this defining property; rather,
+    such implementation is left entirely up to the concrete subclasses.
 
-    This base class specifies the following basic functionality:
-    --> An embedded arc is always oriented in the sense that it will always
-        have a chosen direction of traversal.
-    --> Endpoints of embedded arcs can be abstractly joined together in
-        pairs. The main use case for such abstract joins is to indicate how
-        several embedded arcs can be connected together to form embedded
-        loops in a triangulation.
+    This base class mandates the following basic functionality:
+    --> A normal chord is always oriented in the sense that it will always
+        have a chosen direction of traversal, and always provides a utility
+        for constructing a copy with the opposite orientation.
+    --> The ends of normal chords can be abstractly joined together in pairs.
+        The main use case for such abstract joins is to indicate how several
+        normal chords can be connected together to form abstract loops.
     """
-    #TODO
+    def __init__(self):
+        """
+        Initialises a normal chord with no other chords joined to its ends.
+        """
+        self._joinedChord = [ None, None ]
+        self._joinedEnd = [ None, None ]
+        return
+
+    def join( self, chordEnd, other, otherEnd ):
+        """
+        Abstractly joins an end of this chord to an end of the other chord.
+
+        For both this chord and the other chord, end 0 is the end where
+        traversal begins, and end 1 is where traversal ends.
+
+        It is possible to join the two ends of this chord to each other, by
+        setting other to be self, and otherEnd to be 1 - chordEnd.
+
+        Precondition:
+        --> The two ends involved in the join are not currently joined to
+            anything.
+        --> We are not attempting to join an end to itself (that is, we do
+            not have both other == self and otherEnd == chordEnd).
+
+        Parameters:
+        --> chordEnd    The end (either 0 or 1) of this chord to be joined to
+                        the other chord.
+        --> other       The other chord that will be joined to this chord at
+                        the given ends.
+        --> otherEnd    The end (either 0 or 1) of the other chord to which
+                        we should join this chord.
+        """
+        self._joinImpl( chordEnd, other, otherEnd )
+        other._joinImpl( otherEnd, self, chordEnd )
+        return
+
+    def _joinImpl( self, chordEnd, other, otherEnd ):
+        self._joinedChord[chordEnd] = other
+        self._joinedEnd[chordEnd] = otherEnd
+        return
+
+    def joinedChord( self, chordEnd ):
+        """
+        Returns the NormalChord that is joined to this chord at the given
+        end, or None if there is no such joined chord.
+        """
+        return self._joinedChord[chordEnd]
+
+    def joinedEnd( self, chordEnd ):
+        """
+        Returns the end of self.joinedChord(chordEnd) that is joined to this
+        chord at the given end, or None if there is no such join.
+        """
+        return self._joinedEnd[chordEnd]
+
+    def unjoin( self, chordEnd ):
+        """
+        Undoes the join (if any) at the given end of this chord.
+
+        If there was a joined chord, then this other chord will be updated
+        automatically (that is, after the join has already been undone from
+        this side, there is no need to call unjoin() from the other side).
+        """
+        other = self._joinedChord[chordEnd]
+        if other is not None:
+            # Before we unjoin on this side, we need to use the join data to
+            # jump to the other side and unjoin there first.
+            other._unjoinImpl( self._joinedEnd[chordEnd] )
+            self._unjoinImpl(chordEnd)
+        return
+
+    def _unjoinImpl( self, chordEnd ):
+        self._joinedChord[chordEnd] = None
+        self._joinedEnd[chordEnd] = None
+        return
+
+    @abstractmethod
+    def reversed(self):
+        """
+        Returns a copy of this normal chord with the opposite orientation.
+
+        The reversed copy does not retain information about how the ends are
+        abstractly joined to other chords.
+        """
+        pass
+
+    #TODO Other abstractmethods?
     pass
 
 
@@ -56,23 +145,21 @@ class SegmentedChord(NormalChord):
     --> for i between 0 and (len(segChord) - 1), inclusive, segChord[i]
         returns the segment at index i of the arc
     """
-    #TODO Rearrange all this old code using the new chord framework.
     def __init__( self, segments ):
         """
-        Initialises an embedded arc consisting of the given segments.
+        Initialises a segmented chord built from the given sequence of
+        OrientedSegment objects.
 
-        The segments should be provided as a sequence of OrientedSegment
-        objects satisfying the following conditions:
+        The given segments should satisfy the following conditions:
         --> The OrientedSegment objects are all defined by the same normal
             surface.
-        --> The OrientedSegment objects appear in the list in the order in
-            which they would be encountered as we traverse this arc.
+        --> The OrientedSegment objects appear in the sequence in the order
+            in which they would be encountered as we traverse this chord.
         --> Each individual OrientedSegment is oriented in the direction of
             traversal.
         """
+        super().__init__()  # Initialise with no other chords joined to ends.
         self._segments = tuple(segments)
-        self._joinedArc = [ None, None ]
-        self._joinedEnd = [ None, None ]
         return
 
     def __contains__( self, segment ):
@@ -90,12 +177,12 @@ class SegmentedChord(NormalChord):
 
     def reversed(self):
         """
-        Returns a copy of this embedded arc with the opposite orientation.
+        Returns a copy of this segmented chord with the opposite orientation.
 
         The reversed copy does not retain information about how the ends are
-        abstractly joined to other arcs.
+        abstractly joined to other chords.
         """
-        return OrientedSegments(
+        return SegmentedChord(
                 [ seg.reversed() for seg in reversed(self._segments) ] )
 
     def endSegment( self, i ):
@@ -112,74 +199,6 @@ class SegmentedChord(NormalChord):
         elif i == 1:
             return self[-1]
         raise KeyError( "Ends are numbered either 0 or 1" )
-
-    def join( self, arcEnd, other, otherEnd ):
-        """
-        Abstractly joins an end of this arc to an end of the other arc.
-
-        For both this arc and the other arc, end 0 is the end where traversal
-        begins, and end 1 is where traversal ends.
-
-        It is possible to join the two ends of this arc to each other, by
-        setting other to be self, and otherEnd to be 1 - arcEnd.
-
-        Precondition:
-        --> The two ends involved in the join are not currently joined to
-            anything.
-        --> We are not attempting to join an edge to itself (that is, we do
-            not have both other == self and otherEnd == arcEnd).
-
-        Parameters:
-        --> arcEnd      The end (either 0 or 1) of this arc to be joined to
-                        the other arc.
-        --> other       The other arc that will be joined to this arc at the
-                        given ends.
-        --> otherEnd    The end (either 0 or 1) of the other arc to which we
-                        should join this arc.
-        """
-        self._joinImpl( arcEnd, other, otherEnd )
-        other._joinImpl( otherEnd, self, arcEnd )
-        return
-
-    def _joinImpl( self, arcEnd, other, otherEnd ):
-        self._joinedArc[arcEnd] = other
-        self._joinedEnd[arcEnd] = otherEnd
-        return
-
-    def joinedArc( self, arcEnd ):
-        """
-        Returns the NormalChord that is joined to this arc at the given end,
-        or None if there is no such joined arc.
-        """
-        return self._joinedArc[arcEnd]
-
-    def joinedEnd( self, arcEnd ):
-        """
-        Returns the end of self.joinedArc(arcEnd) that is joined to this arc
-        at the given end, or None if there is no such join.
-        """
-        return self._joinedEnd[arcEnd]
-
-    def unjoin( self, arcEnd ):
-        """
-        Undoes the join (if any) at the given end of this arc.
-
-        If there was a joined arc, then this other arc will be updated
-        automatically (that is, after the join has already been undone from
-        this side, there is no need to call unjoin() from the other side).
-        """
-        other = self._joinedArc[arcEnd]
-        if other is not None:
-            # Before we unjoin on this side, we need to use the join data to
-            # jump to the other side and unjoin there first.
-            other._unjoinImpl( self._joinedEnd[arcEnd] )
-            self._unjoinImpl(arcEnd)
-        return
-
-    def _unjoinImpl( self, arcEnd ):
-        self._joinedArc[arcEnd] = None
-        self._joinedEnd[arcEnd] = None
-        return
 
 
 class InternalSegmentedChord(SegmentedChord):
