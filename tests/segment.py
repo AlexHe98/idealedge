3b68c702706466f899b24cfac51bad1b5f715506
alex-he <mr.a.he98@gmail.com>
2026-08-24 12:22:36 +1000

    Test orientation-tracking (part 2).

--- tests/segment.py
+++ tests/segment.py
@@ -2,15 +2,35 @@
 Test suite for OrientedSegment objects.
 """
 from regina import *
-from segment import OrientedSegment
+from segment import OrientedSegment as Seg
 from sys import argv
 from tests.aux import parseTestNames, allTestsPassedMessage
 from tests.aux import doTest, runNamedTestSuite
 
 
 def testTranslateAlongSurface():
-    #TODO
-    return
+    count = 0
+
+    # One-tetrahedron solid torus
+    tri = Triangulation3("b3Na")    # 1-tet solid torus
+    quadVertexSurfs = NormalSurfaces( tri, NormalCoords.Quad )
+    ann, mob, disc = quadVertexSurfs
+    testCases = [
+            ( "Annulus, type-1 segment", Seg(ann,1,0,1),
+             { Seg(ann,1,2,1), Seg(ann,1,2,-1), Seg(ann,2,0,-1), Seg(ann,2,2,1) },
+             Seg(ann,1,2,-1) ),
+            ( "Annulus, type-2 segment", Seg(ann,1,1,1),
+             { Seg(ann,2,1,1), Seg(ann,2,1,-1) }, Seg(ann,2,1,-1) ),
+            ( "Mobius, type-1 segment", Seg(mob,0,0,-1),
+             { Seg(mob,0,1,-1), Seg(mob,0,1,1), Seg(mob,2,0,1), Seg(mob,2,1,-1) },
+             Seg(mob,0,1,1) ),
+            ( "Disc, type-1 segment", Seg(disc,0,3,1),
+             { Seg(disc,1,1,-1), Seg(disc,1,1,1), Seg(disc,2,1,-1) },
+             Seg(disc,1,1,1) ) ]
+    for name, seg, targets, expected in testCases:
+        doTest( name, expected, seg.translateAlongSurface(targets) )
+        count += 1
+    return count
 
 
 if __name__ == "__main__":
