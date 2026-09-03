f08d1a8297baf6ef935966c06a8e584d30fafb1e
alex-he <mr.a.he98@gmail.com>
2026-09-03 12:11:53 +1000

    Generate non-standard SFS triangulations (part 4).

--- experiments/scratch.py
+++ experiments/scratch.py
@@ -2,17 +2,16 @@
 Scratch work for experimental testing of recogniseSFS().
 
 Usage: Must supply the following integer input arguments:
-    (1) Number of workers
-    (2) Signed genus of the base surface
-    (3) Number of boundary components
-    (4) Number of exceptional fibres
-    (5) Maximum multiplicity of fibres
-    (6) Maximum number of randomisation attempts for each SFS
+    (1) Signed genus of the base surface
+    (2) Number of boundary components
+    (3) Number of exceptional fibres
+    (4) Maximum multiplicity of fibres
+    (5) Maximum number of randomisation attempts for each SFS
 """
 import sys
 from math import gcd as pythonGCD
 from itertools import combinations_with_replacement as combinsWithRep
-from multiprocessing import Pool, TimeoutError
+from multiprocessing import Pool, TimeoutError, cpu_count
 from timeit import default_timer
 from collections import Counter
 from regina import *
@@ -78,24 +77,58 @@ def attemptHardSFS( baseSignedGenus, boundaries, fibres, attempts ):
     return None
 
 
+def _hardSFSPath( isBaseOrbl, genus, numBdries ):
+    if isBaseOrbl:
+        base = "baseOr"
+    else:
+        base = "baseNor"
+    return "sfs-samples/sfs-{}-g{}-m{}.txt".format( base, genus, numBdries )
+
+
+def _sortedFibreParams(fibreParams):
+    return tuple( sorted(fibreParams) )
+
+
+def _sortedFibreNegation(fibreParams):
+    return _sortedFibreParams(
+            (2, 1) if p == 2 else (p, -q) for p, q in fibreParams )
+
+
+def _readSFSLine(line):
+    data = line.rstrip().split(" ")
+    fibres = []
+    neoSig = data[0]
+    for fibreString in data[1:]:
+        fibres.append( tuple(
+            int(k) for k in fibreString[1:-1].split(",") ) )
+    return neoSig, tuple(fibres)
+
+
+def _sfsLine( neoSig, fibres ):
+    line = neoSig
+    for p, q in fibres:
+        line += " ({},{})".format( p, q )
+    return line + "\n"
+
+
 if __name__ == "__main__":
-    numWorkers = int( sys.argv[1] )
-    baseSignedGenus = int( sys.argv[2] )
-    numBdries = int( sys.argv[3] )
+    baseSignedGenus = int( sys.argv[1] )
+    numBdries = int( sys.argv[2] )
     if numBdries < 1:
         raise ValueError( "Can't have {} boundaries. ".format(numBdries) +
                          "Must be at least 1." )
-    numFibres = int( sys.argv[4] )
+    numFibres = int( sys.argv[3] )
     if numFibres < 0:
         raise ValueError( "Can't have {} ".format(numFibres) +
                          "exceptional fibres. Must be at least 0." )
-    maxMultiplicity = int( sys.argv[5] )
+    maxMultiplicity = int( sys.argv[4] )
     if maxMultiplicity < 2:
         raise ValueError( "Can't have exceptional fibres of " +
                          "multiplicity {}. ".format(maxMultiplicity) +
                          "Must be at least 2." )
-    maxAttempts = int( sys.argv[6] )
-    if baseSignedGenus >= 0:
+    maxAttempts = int( sys.argv[5] )
+    isBaseOrbl = ( baseSignedGenus >=  0 )
+    if isBaseOrbl:
         orbl = "Orientable"
         baseClass = SFSpace.Class.bo1
         baseGenus = baseSignedGenus
@@ -103,7 +136,6 @@ if __name__ == "__main__":
         orbl = "Non-orientable"
         baseClass = SFSpace.Class.bn2
         baseGenus = -baseSignedGenus
-    print( "Workers: {}".format(numWorkers) )
     print( "{} base, genus {} with {} boundaries.".format(
         orbl, baseGenus, numBdries ) )
     print( "{} fibres with max multiplicity {}.".format(
@@ -114,59 +146,84 @@ if __name__ == "__main__":
         print( "Max {} randomisation attempts per SFS.".format(maxAttempts) )
     print()
     sys.stdout.flush()
-    start = default_timer()
-    fibres = []
-    for p in range( 2, 1 + maxMultiplicity ):
-        for q in range( 1, 1 + p//2 ):
-            if pythonGCD(p, q) != 1:
-                continue
-            fibres.append( (p, q) )
-            if p != 2:
-                fibres.append( (p, -q) )
-
-    # Try to generate a non-standard triangulation for each Seifert fibred
-    # space satisfying the given parameters.
-    manifold = dict()
-    unfinished = dict()
-    with Pool( processes=numWorkers ) as pool:
+    filePath = _hardSFSPath( isBaseOrbl, baseGenus, numBdries )
+    with open( filePath, 'a+' ) as sfsFile:
+        # First read in all the manifolds where we have previously found
+        # non-standard triangulations.
+        foundParams = set()
         sortedNegations = set()
+        sfsFile.seek(0)
+        for line in sfsFile:
+            _, fibres = _readSFSLine(line)
+            foundParams.add(fibres)
+            sortedNegations.add( _sortedFibreNegation(fibres) )
+
+        # For each SFS satisfying the given parameters, if we haven't already
+        # found a non-standard triangulation, then try to generate one now.
+        start = default_timer()
+        fibres = []
+        for p in range( 2, 1 + maxMultiplicity ):
+            for q in range( 1, 1 + p//2 ):
+                if pythonGCD(p, q) != 1:
+                    continue
+                fibres.append( (p, q) )
+                if p != 2:
+                    fibres.append( (p, -q) )
+        paramsToAttempt = dict()
+        manifold = dict()
         count = 0
         for fibreParams in combinsWithRep( fibres, numFibres ):
-            # Avoid duplicates with the opposite orientation.
-            if tuple( sorted(fibreParams) ) in sortedNegations:
+            if fibreParams in foundParams:
                 continue
-            sortedNegations.add( tuple( sorted(
-                (2, 1) if p == 2 else (p, -q) for p, q in fibreParams ) ) )
 
-            # We have a bounded orientable SFS for which we hope to find a
-            # non-standard triangulation.
+            # Avoid duplicates with the opposite orientation.
+            if _sortedFibreParams(fibreParams) in sortedNegations:
+                continue
+            sortedNegations.add( _sortedFibreNegation(fibreParams) )
+
+            # We have a bounded orientable SFS for which we hope to find
+            # a non-standard triangulation.
+            paramsToAttempt[count] = fibreParams
             manifold[count] = SFSpace( baseClass, baseGenus, numBdries )
             for p, q in fibreParams:
                 manifold[count].insertFibre( p, q )
-            unfinished[count] = pool.apply_async( attemptHardSFS, args = (
-                baseSignedGenus, numBdries, fibreParams, maxAttempts ) )
             count += 1
-        print( "Generating up to {} triangulations".format(count) )
+
+        # Do as many in parallel as possible.
+        numWorkers = min( count, max( 1, cpu_count() - 1 ) )
+        print( "Time: {:.6f}".format( default_timer() - start ) )
+        print( "Generating up to {} new triangulations".format(count) )
+        print( "Using {} workers".format(numWorkers) )
         print()
         sys.stdout.flush()
+        unfinished = dict()
+        with Pool( processes=numWorkers ) as pool:
+            for i in range(count):
+                unfinished[i] = pool.apply_async( attemptHardSFS, args = (
+                    baseSignedGenus, numBdries,
+                    paramsToAttempt[i], maxAttempts ) )
 
-        # Repeatedly poll for new results.
-        found = 0
-        while unfinished:
-            nowFinished = set()
-            for i in unfinished:
-                try:
-                    output = unfinished[i].get( timeout=0.001 )
-                except TimeoutError:
-                    pass
-                else:
-                    # Got a new result!
-                    nowFinished.add(i)
+            # Repeatedly poll for new results.
+            found = 0
+            while unfinished:
+                nowFinished = set()
+                for i in unfinished:
+                    try:
+                        output = unfinished[i].get( timeout=0.001 )
+                    except TimeoutError:
+                        pass
+                    else:
+                        # Got a new result!
+                        nowFinished.add(i)
 
-                    # Did we actually find a hard SFS triangulation?
-                    if output is not None:
-                        found += 1
-                        print( "#{}. Time: {:.6f}. {}. \"{}\".".format(
-                            found, default_timer() - start, manifold[i], output ) )
-            for i in nowFinished:
-                del unfinished[i]
+                        # Did we actually find a hard SFS triangulation?
+                        if output is not None:
+                            #TODO Write to file.
+                            found += 1
+                            print( "#{}. Time: {:.6f}. {}. \"{}\".".format(
+                                found, default_timer() - start, manifold[i], output ) )
+                            sys.stdout.flush()
+                for i in nowFinished:
+                    del unfinished[i]
+    print()
+    print( "Time: {:.6f}".format( default_timer() - start ) )
