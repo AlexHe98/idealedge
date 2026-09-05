fa9d873252c44aad092cd0a9107afbece1ac89bd
alex-he <mr.a.he98@gmail.com>
2026-09-05 14:23:30 +1000

    Run experiments on non-standard SFS triangulations (part 2).

--- experiments/sfs/io.py
+++ experiments/sfs/io.py
@@ -37,7 +37,7 @@ def readSFSLine(line):
     return neoSig, tuple(fibres)
 
 
-def hardSFSPath( isBaseOrbl, genus, numBdries ):
+def hardSFSPath( isBaseOrbl=None, genus=None, numBdries=None ):
     """
     Returns the path to a file containing non-standard triangulations of
     bounded orientable Seifert fibred spaces with the given properties.
@@ -45,9 +45,57 @@ def hardSFSPath( isBaseOrbl, genus, numBdries ):
     If such a file exists, then it should have been generated
     programmatically, and each line of the file should follow the format
     described in sfsLine().
+
+    If no properties are provided, then this routine simply returns the path
+    to the directory containing all such files.
     """
-    if isBaseOrbl:
+    if isBaseOrbl is None:
+        return "sfs-samples/"
+    elif isBaseOrbl:
         base = "baseOr"
     else:
         base = "baseNor"
+    if (genus is None) or (numBdries is None):
+        raise ValueError( "hardSFSPath() requires that either all three " +
+                         "properties are supplied, or none at all" )
     return "sfs-samples/sfs-{}-g{}-m{}.txt".format( base, genus, numBdries )
+
+
+def parseSFSFileName(fileName):
+    """
+    Parses a file name of the format 'sfs-<base>-g<genus>-m<numBdries>.txt'.
+
+    This routine returns a tuple (isBaseOrbl, genus, numBdries), where:
+    --> isBaseOrbl is True if <base> is "baseOr", and False if <base> is
+        "baseNor";
+    --> genus is a non-negative (moreover, positive if isBaseOrbl is False)
+        integer; and
+    --> numBdries is a positive integer.
+    """
+    if fileName[-4:] != ".txt":
+        raise ValueError(
+                "parseSFSFileName() requires '.txt' file extension" )
+    split = fileName[:-4].split("-")
+    msg = "Incorrect format for parseSFSFileName()"
+    if ( len(split) != 4 or split[0] != "sfs" or split[1][:4] != "base" or
+        split[2][0] != "g" or split[3][0] != "m" ):
+        raise ValueError(msg)
+    try:
+        # Because we split at "-" characters, any integers we get should be
+        # non-negative.
+        genus = int( split[2][1:] )
+        numBdries = int( split[3][1:] )
+    except Exception:
+        raise ValueError(msg)
+    if split[1][4:] == "Or":
+        isBaseOrbl = True
+    elif split[1][4:] == "Nor":
+        isBaseOrbl = False
+        if genus == 0:
+            raise ValueError(
+                    "Non-orientable base surface cannot have genus 0" )
+    else:
+        raise ValueError(msg)
+    if numBdries == 0:
+        raise ValueError( "Number of boundaries should be positive" )
+    return (isBaseOrbl, genus, numBdries)
--- experiments/sfs/scratch.py
+++ experiments/sfs/scratch.py
@@ -1,25 +1,28 @@
 """
 Scratch work for SFS experiments.
 """
+from pathlib import Path
 from timeit import default_timer
 from regina import *
 from recsfs import recogniseSFS
-from experiments.sfs.io import hardSFSPath, readSFSLine
+from experiments.sfs.io import hardSFSPath, readSFSLine, parseSFSFileName
 
 
 if __name__ == "__main__":
-    #TODO
-    isBaseOrbl = False
-    baseGenus = 4
-    numBdries = 4
-    filePath = hardSFSPath( isBaseOrbl, baseGenus, numBdries )
-    with open( filePath, 'r' ) as sfsFile:
-        for line in sfsFile:
-            neoSig, fibres = readSFSLine(line)
-            tri = Triangulation3.fromSig(neoSig)
-            size = tri.size()
-            #TODO Compare computed vs expected results.
-            start = default_timer()
-            ans = recogniseSFS(tri)
-            time = default_timer() - start
-            print( "Size: {}. Time: {:.6f}. {}".format( size, time, ans ) )
+    for file in Path( hardSFSPath() ).iterdir():
+        if file.is_file():
+            isBaseOrbl, genus, numBdries = parseSFSFileName(file.name)
+            with open( hardSFSPath(
+                isBaseOrbl, genus, numBdries ) ) as sfsFile:
+                for line in sfsFile:
+                    neoSig, fibres = readSFSLine(line)
+                    tri = Triangulation3.fromSig(neoSig)
+                    size = tri.size()
+                    #TODO Compare computed vs expected results.
+                    start = default_timer()
+                    ans = recogniseSFS(tri)
+                    time = default_timer() - start
+                    print( "Size: {}. Time: {:.6f}. {}".format(
+                        size, time, ans ) )
+            #TODO Temporarily break out while testing.
+            break
