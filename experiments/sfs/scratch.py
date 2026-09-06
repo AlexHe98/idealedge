a483e9b28901bb34a1b56449aa80485e9d1db4d4
alex-he <mr.a.he98@gmail.com>
2026-09-06 15:10:18 +1000

    Write SFS experiment data to file.

--- experiments/sfs/scratch.py
+++ experiments/sfs/scratch.py
@@ -2,6 +2,7 @@
 Scratch work for SFS experiments.
 """
 from pathlib import Path
+import sys
 from timeit import default_timer
 from regina import *
 from aux.sfs import fibrePreservingHomeomorphic
@@ -10,33 +11,37 @@ from experiments.sfs.io import hardSFSPath, readSFSLine, parseSFSFileName
 
 
 if __name__ == "__main__":
+    outputPath = sys.argv[1]
     useHeuristics = True
-    for file in Path( hardSFSPath() ).iterdir():
-        if file.is_file():
-            isBaseOrbl, genus, numBdries = parseSFSFileName(file.name)
-            if isBaseOrbl:
-                baseClass = SFSpace.Class.bo1
-            else:
-                baseClass = SFSpace.Class.bn2
-            with open( hardSFSPath(
-                isBaseOrbl, genus, numBdries ) ) as sfsFile:
-                for line in sfsFile:
-                    neoSig, fibres = readSFSLine(line)
-                    tri = Triangulation3.fromSig(neoSig)
-                    size = tri.size()
-                    tracker = SFSRecognitionTracker()
-                    start = default_timer()
-                    ans = recogniseSFS( tri, useHeuristics, tracker )
-                    time = default_timer() - start
-                    print( "Size: {}. Time: {:.6f}. #altEnums: {}. {}".format(
-                        size, time,
-                        tracker.alternateEnumerationsCount(), ans ) )
+    with open( outputPath, 'x' ) as outputFile:
+        for file in Path( hardSFSPath() ).iterdir():
+            if file.is_file():
+                isBaseOrbl, genus, numBdries = parseSFSFileName(file.name)
+                if isBaseOrbl:
+                    baseClass = SFSpace.Class.bo1
+                else:
+                    baseClass = SFSpace.Class.bn2
+                with open( hardSFSPath(
+                    isBaseOrbl, genus, numBdries ) ) as sfsFile:
+                    for line in sfsFile:
+                        neoSig, fibres = readSFSLine(line)
+                        tri = Triangulation3.fromSig(neoSig)
+                        size = tri.size()
+                        tracker = SFSRecognitionTracker()
+                        start = default_timer()
+                        ans = recogniseSFS( tri, useHeuristics, tracker )
+                        time = default_timer() - start
+                        msg = "{} {} {:.6f}".format(
+                                size, tracker.alternateEnumerationsCount(),
+                                time )
+                        outputFile.write( msg + "\n" )
+                        outputFile.flush()
+                        print(msg)
+                        sys.stdout.flush()
 
-                    # Check that we actually got the correct answer.
-                    expected = SFSpace( baseClass, genus, numBdries )
-                    for p, q in fibres:
-                        expected.insertFibre( p, q )
-                    assert fibrePreservingHomeomorphic( expected, ans ),\
-                            expected
-            #TODO Temporarily break out while testing.
-            break
+                        # Check that we actually got the correct answer.
+                        expected = SFSpace( baseClass, genus, numBdries )
+                        for p, q in fibres:
+                            expected.insertFibre( p, q )
+                        assert fibrePreservingHomeomorphic( expected, ans ),\
+                                expected
