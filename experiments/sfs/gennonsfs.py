ddff4100c0c1c383fdab42f194896b38fe2aa5ac
alex-he <mr.a.he98@gmail.com>
2026-09-18 09:15:41 +1000

    Yet more preliminary experiments.

--- experiments/sfs/gennonsfs.py
+++ experiments/sfs/gennonsfs.py
@@ -18,47 +18,39 @@ FIBRE_LIST = [ (2,1), (3,1), (3,-1), (4,1), (4,-1),
               (5,1), (5,2), (5,-1), (5,-2) ]
 
 
-if __name__ == "__main__":
-    # Generate a random hyperbolic 3-manifold with one torus boundary
-    # component, together with a random SFS with two torus boundary
-    # components, and glue them together.
-    tri = dict()
-    hyp = snappy.OrientableCuspedCensus(num_cusps=1).random()
-    tri[1] = Triangulation3( hyp.triangulation_isosig(decorated=False) )
-    tri[1].idealToFinite()
-    tri[1].simplify()
-    tri[1].minimiseBoundary()
-    RandomEngine.reseedWithHardware()
-    baseSignedGenus = GENUS_LIST[ RandomEngine.rand( len(GENUS_LIST) ) ]
-    numBdries = 2
-    numFibres = 2 + RandomEngine.rand(3)
-    fibres = [ FIBRE_LIST[ RandomEngine.rand( len(FIBRE_LIST) ) ]
-              for _ in range(numFibres) ]
-    tri[2] = orientableSFS( baseSignedGenus, numBdries, *fibres )
-    tri[2].simplify()
-    for i in [1, 2]:
-#        mfd = snappy.OrientableCuspedCensus(num_cusps=i).random()
-#        tri[i] = Triangulation3( mfd.triangulation_isosig(decorated=False) )
-#        tri[i].idealToFinite()
-#        tri[i].simplify()
-#        tri[i].minimiseBoundary()
-        # Try really hard to simplify.
-        simplifiedNow = True
-        while simplifiedNow:
-            simplifiedNow = tri[i].simplify()
-            if not simplifiedNow:
-                simplifiedNow = tri[i].simplify()
+def _simplifyHard(tri):
+    # Try really hard to simplify.
+    simplifiedNow = True
+    while simplifiedNow:
+        simplifiedNow = tri.simplify()
+        if not simplifiedNow:
+            simplifiedNow = tri.simplify()
+    return
+
+
+def _randomHyp(numBdries):
+    mfd = snappy.OrientableCuspedCensus(num_cusps=numBdries).random()
+    tri = Triangulation3( mfd.triangulation_isosig(decorated=False) )
+    tri.idealToFinite()
+    tri.simplify()
+    tri.minimiseBoundary()
+    _simplifyHard(tri)
+    return tri
+
+
+def _glueTogether( myTri, yourTri, simplify=True ):
+    glued = Triangulation3(myTri)
+    glued.insertTriangulation( yourTri )
 
     # For now, just arbitrarily pick an easy gluing.
-    myFront = tri[1].boundaryComponent(0).edge(0).front()
+    myFront = glued.boundaryComponent(0).edge(0).front()
     myTeti = myFront.tetrahedron().index()
     myEn = myFront.edge()
-    yourFront = tri[2].boundaryComponent(0).edge(0).front()
-    yourTeti = tri[1].size() + yourFront.tetrahedron().index()
+    yourFront = yourTri.boundaryComponent(0).edge(0).front()
+    yourTeti = myTri.size() + yourFront.tetrahedron().index()
     yourEn = yourFront.edge()
-    tri[1].insertTriangulation( tri[2] )
-    myEdge = tri[1].tetrahedron(myTeti).edge(myEn)
-    yourEdge = tri[1].tetrahedron(yourTeti).edge(yourEn)
+    myEdge = glued.tetrahedron(myTeti).edge(myEn)
+    yourEdge = glued.tetrahedron(yourTeti).edge(yourEn)
     myFront = myEdge.front()
     yourFront = yourEdge.front()
     myBack = myEdge.back()
@@ -71,15 +63,33 @@ if __name__ == "__main__":
             myBack.vertices()[2],
             yourBack.tetrahedron(),
             yourBack.vertices() * myBack.vertices().inverse() )
+    if simplify:
+        _simplifyHard(glued)
+    return glued
 
-    # Might as well simplify.
-    simplifiedNow = True
-    while simplifiedNow:
-        simplifiedNow = tri[1].simplify()
-        if not simplifiedNow:
-            simplifiedNow = tri[1].simplify()
-    print( f"Size: {tri[1].size()}" )
-    print( tri[1].isoSig() )
+
+if __name__ == "__main__":
+    # Build inner triangulation from random hyperbolic 3-manifold(s)
+    inner = _randomHyp(1)
+#    hyp = dict()
+#    for hypNumBdries in [1, 2]:
+#        hyp[hypNumBdries] = _randomHyp(hypNumBdries)
+#    inner = _glueTogether( hyp[1], hyp[2] )
+
+    # Glue the inner triangulation to a random SFS.
+    RandomEngine.reseedWithHardware()
+    baseSignedGenus = GENUS_LIST[ RandomEngine.rand( len(GENUS_LIST) ) ]
+    sfsNumBdries = 2
+    numFibres = 2 + RandomEngine.rand(3)
+    fibres = [ FIBRE_LIST[ RandomEngine.rand( len(FIBRE_LIST) ) ]
+              for _ in range(numFibres) ]
+    sfsTri = orientableSFS( baseSignedGenus, sfsNumBdries, *fibres )
+    _simplifyHard(sfsTri)
+    tri = _glueTogether( sfsTri, inner )
+    assert tri.isConnected()
+    assert tri.countBoundaryComponents() == 1
+    print( f"Size: {tri.size()}" )
+    print( tri.isoSig() )
     print()
 
     # Test.
@@ -89,6 +99,6 @@ if __name__ == "__main__":
     useHeuristics = True
     tracker = SFSRecognitionTracker()
     start = default_timer()
-    print( recogniseSFS( tri[1], useHeuristics, tracker ) )
+    print( recogniseSFS( tri, useHeuristics, tracker ) )
     print( "Time: {:.6f}".format( default_timer() - start ) )
     print( "Alternate enumerations:", tracker.alternateEnumerationsCount() )
